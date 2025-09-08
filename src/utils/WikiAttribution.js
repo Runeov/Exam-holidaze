d35e83c6f903d2cacd39895c23495670fac01974
Per Rune Overelv <100531314+Runeov@users.noreply.github.com>
2025-09-08 05:00:31 +0300

    Cleaning old files
    Cleaning old files

--- src/utils/WikiAttribution.js
+++
@@ -1,5 +0,0 @@
-// utils/wikiAttribution.js
-export function buildPermalink(title, oldid) {
-  if (!title || !oldid) return `https://en.wikivoyage.org/wiki/${encodeURIComponent(title)}`;
-  return `https://en.wikivoyage.org/w/index.php?title=${encodeURIComponent(title)}&oldid=${oldid}`;
-}
